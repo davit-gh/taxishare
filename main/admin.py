35e3a7531eacb2926442978e1bd555300f3583ba
davit-gh <davsmile@yahoo.com>
2015-01-06 20:24:16 +0400

    add navbar to account page, add wow js - medium size upggrade

--- main/admin.py
+++ main/admin.py
@@ -16,7 +16,7 @@ class CancellationAdmin(TabularDynamicInlineAdmin):
 
 class EventAdmin(admin.ModelAdmin):
 	inlines = [CancellationAdmin,]
-	list_display=('source','destination','start_date','end_date', 'repeat', 'end_repeat', 'title', 'passanger_number')
+	list_display=('source','destination','start_date','created_by', 'repeat', 'end_repeat', 'title', 'passanger_number')
 
 admin.site.register(Event, EventAdmin)
 #admin.site.register(SourceDest, OrderAdmin)
--- urls.py
+++ urls.py
@@ -50,7 +50,7 @@ urlpatterns += solid_i18n_patterns('',
     # one homepage pattern, so if you use a different one, comment this
     # one out.
 
-    url("^$", direct_to_template, {"template": "index.html"}, name="home"),
+    #url("^$", direct_to_template, {"template": "index.html"}, name="home"),
 
     
     # HOMEPAGE AS AN EDITABLE PAGE IN THE PAGE TREE
@@ -69,7 +69,7 @@ urlpatterns += solid_i18n_patterns('',
     # "/.html" - so for this case, the template "pages/index.html" can
     # be used.
 
-    # url("^$", "mezzanine.pages.views.page", {"slug": "/"}, name="home"),
+    url("^$", "mezzanine.pages.views.page", {"slug": "/"}, name="home"),
 
     # HOMEPAGE FOR A BLOG-ONLY SITE
     # -----------------------------
